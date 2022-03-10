0845934fc43203c06472acc0dd7b6f780c3b8df1
marcinzygan <soul@o2.pl>
2022-03-10 22:47:27 +0000

    added dynamic cards with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App(){
     return(
-        <div>
+    <div>
         <Navbar/>
         <Hero />
-        </div>
+        <section className="section__cards">
+      <div className="card__grid">
+        <Card 
+           img="card.png"
+           rating={5.0.toFixed(1)}
+           reviewCount={6}
+           country="Poland"
+           title="Life lessons with Katie Zaferes"
+           price={136}
+        />
+         <Card 
+           img="cookie.jpg"
+           rating={4.7.toFixed(1)}
+           reviewCount={123}
+           country="Poland"
+           title="Cookies baking class"
+           price={25}
+        />
+         <Card 
+           img="wedding.png"
+           rating={4.9.toFixed(1)}
+           reviewCount={12}
+           country="USA"
+           title="Have a great wedding day!"
+           price={2000}
+        />
+      </div>
+      </section>
+    </div>
     )
 }
