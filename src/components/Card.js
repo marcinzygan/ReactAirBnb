119ace46171eaa1037dc68b52e578f0b2d3f807a
marcinzygan <soul@o2.pl>
2022-03-10 22:48:00 +0000

    added dynamic cards with props

---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import star from "./images/star.png"
+
+
+export default function Card (props){
+    console.log(props);
+    
+    return(
+        
+        <div className="card">
+            <div className="card__img-container">
+               <img src={require(`./images/${props.img}`)} className="card__img"></img>
+               <p className="card__status">Sold Out</p>
+            </div>
+            <div className="card__info">
+                <div className="card__stats">
+                <img src={star} className="card__star"></img>
+                <p className="rating">{props.rating}</p>
+                <p className="stock">({props.reviewCount})</p>
+                <p className="country">{props.country}</p>
+                </div>
+                <p className="card__title">{props.title}</p>
+                <p className="card__price-data"><span className="card__price">From ${props.price}</span> / person</p>
+            </div>
+        </div>
+       
+    )
+}
